e8756fc9c76e721e22a93ff3edaf71e7327ba361
Henrique-Arau <penielh43@gmail.com>
2024-02-14 18:07:04 -0300

    criando outras paginas

---
+++ angular-blog/src/app/pages/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo": "aaa",
+        "title": "Saiu novo filme",
+        "description": "Olá mundo"
+    },
+    {
+        "id":2,
+        "photo": "bbb",
+        "title": "Saiu uma nova serie",
+        "description": "Olá"
+    }
+]
+ 
